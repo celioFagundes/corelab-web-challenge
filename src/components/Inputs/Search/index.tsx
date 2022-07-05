0db95de21e597f12692676d35631477eaf6fe9ed
Célio <celiofagundes30@gmail.com>
2022-07-05 14:24:32 -0300

    feat: created search input

--- src/components/Inputs/Search/index.tsx
+++ src/components/Inputs/Search/index.tsx
@@ -1,12 +1,21 @@
+import { ChangeEventHandler } from 'react'
+import { SearchIcon } from '../../Icons'
 import styles from './style.module.scss'
 interface ISearch {
   placeholder: string
   value: string
-  onChange: () => void
+  onChange: ChangeEventHandler<HTMLInputElement>
 }
 
 const Search = (props: ISearch) => {
-  return <input type='text' placeholder={props.placeholder} value={props.value} />
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.search_icon}>
+        <SearchIcon />
+      </div>
+      <input className={styles.input} onChange = {props.onChange} type='text' placeholder={props.placeholder} value={props.value} />
+    </div>
+  )
 }
 
 export { Search }
