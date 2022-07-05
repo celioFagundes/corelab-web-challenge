7869892f0560a09f33c54a63eee8330770c8e2ee
Célio <celiofagundes30@gmail.com>
2022-07-05 14:25:11 -0300

    feat: added search input logic, styles

--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -1,40 +1,51 @@
-import { useEffect, useState } from "react";
-import { getVehicles } from "../../lib/api";
-import { Button, Card } from "../../components";
-import { Search } from "../../components/Inputs";
-import styles from "./Vehicles.module.scss";
-import { IVehicle } from "../../types/Vehicle";
+import { useEffect, useState } from 'react'
+import { getVehicles } from '../../lib/api'
+import { Button, Card } from '../../components'
+import { Search } from '../../components/Inputs'
+import styles from './Vehicles.module.scss'
+import { IVehicle } from '../../types/Vehicle'
+import { FilterIcon } from '../../components/Icons'
 
 const VehiclesPage = () => {
-  const [vehicles, setVehicles] = useState<IVehicle[]>([]);
-  const [search, setSearch] = useState<string>("");
+  const [vehicles, setVehicles] = useState<IVehicle[]>([])
+  const [search, setSearch] = useState<string>('')
 
   useEffect(() => {
     const fetchVehicles = async () => {
-      const payload = await getVehicles();
-      setVehicles(payload);
-    };
+      const payload = await getVehicles()
+      setVehicles(payload)
+    }
 
-    fetchVehicles();
-  }, []);
+    fetchVehicles()
+  }, [])
 
-  console.log({ vehicles });
+  const handleInputSearch = (value: string) => {
+    setSearch(value)
+  }
 
   return (
     <div className={styles.Vehicles}>
       <main className={styles.main}>
-        <Search placeholder="Search" value={search} onChange={() => {}} />
+        <div className={styles.search_filter_box}>
+          <Search
+            placeholder='Buscar'
+            value={search}
+            onChange={evt => handleInputSearch(evt.target.value)}
+          />
+          <button className={styles.filter_button}>
+            <FilterIcon />
+          </button>
+        </div>
 
-        <Button text="Add new vehicle" onClick={() => {}} />
-
-        <Card title="Sandero Stepway">
+        <Button text='Add new vehicle' onClick={() => {}} />
+        <Card title='Sandero Stepway'>
           <p>Price: 22000</p>
           <p>Description: Carro usado por 2 anos...</p>
           <p>Year: 2018</p>
         </Card>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default VehiclesPage;
+export default VehiclesPage
