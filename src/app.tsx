df2b619475901873ccdcb0681a3b81d01ac987a0
Célio <celiofagundes30@gmail.com>
2022-07-06 11:31:05 -0300

    feat: added edit page route

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CreateVehicle from './pages/Vehicles/Create'
+import EditVehicle from './pages/Vehicles/Edit'
 import HomeVehicles from './pages/Vehicles/Home'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomeVehicles />} />
         <Route path='/vehicles/create' element={<CreateVehicle />} />
+        <Route path='/vehicles/edit/:id' element={<EditVehicle />} />
       </Routes>
     </BrowserRouter>
   )
