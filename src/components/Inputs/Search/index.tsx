13ff1a99b0ec333a5849ac36eaee3b138d97c73b
Célio <celiofagundes30@gmail.com>
2022-07-08 15:58:16 -0300

    feat: added search functions, refactored props, added keyword display

--- src/components/Inputs/Search/index.tsx
+++ src/components/Inputs/Search/index.tsx
@@ -1,19 +1,49 @@
-import { ChangeEventHandler } from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai'
 import styles from './style.module.scss'
 interface ISearch {
-  placeholder: string
-  value: string
-  onChange: ChangeEventHandler<HTMLInputElement>
+  onSubmit: (slug: string) => void
+  currentKeyword: string
 }
 
 const Search = (props: ISearch) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleKeyDown = (keyboardEvent: React.KeyboardEvent<HTMLInputElement>) => {
+    if (keyboardEvent.code === 'Enter') {
+      props.onSubmit(inputValue)
+    }
+  }
+  const handleInputChange = (text: string) => {
+    setInputValue(text)
+  }
+  const resetSearch = () => {
+    setInputValue('')
+    props.onSubmit('')
+  }
   return (
     <div className={styles.wrapper}>
-      <div className={styles.search_icon}>
-        <AiOutlineSearch/>
+      <div className={styles.input_wrapper}>
+        <div className={styles.search_icon}>
+          <AiOutlineSearch />
+        </div>
+        <input
+          className={styles.input}
+          onKeyDown={handleKeyDown}
+          onChange={evt => handleInputChange(evt.target.value)}
+          type='text'
+          placeholder='Digite o termo e pressione a tecla enter para pesquisar'
+          value={inputValue}
+        />
       </div>
-      <input className={styles.input} onChange = {props.onChange} type='text' placeholder={props.placeholder} value={props.value} />
+      {props.currentKeyword !== '' && (
+        <div className={styles.results_wrapper}>
+          <p className={styles.results_text}>Mostrando resultados para "{props.currentKeyword}"</p>
+          <button onClick={resetSearch} className={styles.results_reset}>
+            <AiOutlineClose size={18} />
+          </button>
+        </div>
+      )}
     </div>
   )
 }
