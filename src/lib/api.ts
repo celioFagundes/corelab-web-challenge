8ef57b00844e4d462c43f9d5558689219efa748b
Célio <celiofagundes30@gmail.com>
2022-07-08 15:59:34 -0300

    feat: refactored get, added put, remove , toggle favorite methods

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,22 @@
-const API = "http://localhost:3333";
-
-const endpoint = (path: string): string => API + path;
+import axios from 'axios'
+const API = 'http://localhost:3333'
+const endpoint = (path: string): string => API + path
 
 const get = async (path: string): Promise<any> => {
-  return fetch(endpoint(path)).then((res) => res.json());
-};
-
-export const getVehicles = async () => {
-  return get("/vehicles");
-};
+  return axios.get(path).then(res => res.data)
+}
+const put = async (path: string): Promise<any> => {
+  return axios.put(endpoint(path))
+}
+const remove = async (path: string): Promise<any> => {
+  return axios.delete(endpoint(path))
+}
+export const getVehicles = async (path: string) => {
+  return get(path)
+}
+export const removeVehicles = async (id: number) => {
+  return remove(`/vehicles/${id}`)
+}
+export const toggleIsFavorite = async (id: number) => {
+  return put(`/vehicles/favorite/${id}`)
+}
