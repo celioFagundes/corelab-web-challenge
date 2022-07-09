56820fa56018e80e439a9038e2f0361a24cbc1a3
Célio <celiofagundes30@gmail.com>
2022-07-08 22:56:32 -0300

    feat: finished create page

--- src/pages/Vehicles/Create/index.tsx
+++ src/pages/Vehicles/Create/index.tsx
@@ -2,28 +2,49 @@ import { useFormik } from 'formik'
 import { ButtonSubmit } from '../../../components/Buttons'
 import styles from './styles.module.scss'
 import * as Yup from 'yup'
-import { Link } from 'react-router-dom'
+import { Link, Navigate, Router, useNavigate } from 'react-router-dom'
 import { BiArrowBack } from 'react-icons/bi'
+import { createVehicle } from '../../../lib/api'
+import { IVehicle } from '../../../types/Vehicle'
 const colorOptions = ['Vermelho', 'Preto', 'Azul', 'Prata', 'Branco']
+const brandOptions = [
+  'Fiat',
+  'Citroen',
+  'Volkswagen',
+  'Chevrolet',
+  'Toyota',
+  'Nissan',
+  'Hyundai',
+  'Honda',
+  'Ford',
+  'Peugeot',
+  'Renault',
+]
 const VehicleSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'Por favor, informe um nome com pelo menos 3 caracteres')
     .required('Por favor, informe o nome do veículo'),
+  description: Yup.string()
+    .min(15, 'Por favor, informe um descrição com pelo menos 10 caracteres')
+    .required('Por favor, informe a descrição do veículo'),
   brand: Yup.string()
     .min(3, 'Por favor, informe um nome com pelo menos 3 caracteres')
     .required('Por favor, informe o nome da marca'),
   color: Yup.string()
     .min(1, 'Por favor, selecione uma cor')
     .required('Por favor, selecione uma cor'),
+  price: Yup.number().min(3000, 'Preço minímo 3.000').required('Por favor informe o ano'),
   year: Yup.number()
-    .test('len', 'O ano deve ter 4 digitos', val => (val ? val.toString().length === 4 : false))
-    .max(new Date().getFullYear(), 'Ano deve ser menor ou igual ao ano atual'),
+    .test('tamanho', 'O ano deve ter 4 digitos', val => (val ? val.toString().length === 4 : false))
+    .max(new Date().getFullYear(), 'Ano deve ser menor ou igual ao ano atual')
+    .required('Por favor informe o ano'),
   plate: Yup.string()
     .trim()
     .matches(/^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$/, 'Digite uma placa válida')
     .required('Por favor, informe a placa do veículo'),
 })
 const CreateVehicle = () => {
+  const navigate = useNavigate()
   const form = useFormik({
     validateOnChange: false,
     validateOnMount: false,
@@ -31,19 +52,18 @@ const CreateVehicle = () => {
     initialValues: {
       name: '',
       brand: '',
+      description: '',
       color: '',
       year: 0,
       plate: '',
+      price: 0,
     },
     validationSchema: VehicleSchema,
     onSubmit: async values => {
-      /*const dateParts = values.birth_date.split('/')
-      const validDate = `${dateParts[1]}/${dateParts[0]}/${dateParts[2]}`
-      const newValues = { ...values, birth_date: validDate }
-      const createData = await axios.post('http://localhost:3000/agents/', newValues)
-      if (createData.status) {
-        router.push('/agents')
-      }*/
+      const createData: IVehicle = await createVehicle(values)
+      if (createData) {
+        navigate('/')
+      }
     },
   })
   return (
@@ -68,15 +88,36 @@ const CreateVehicle = () => {
           <p className={styles.error_message}>{form.errors.name}</p>
         </div>
         <div className={styles.input_wrapper}>
-          <label className={styles.label}>Marca</label>
+          <label className={styles.label}>Descrição</label>
           <input
-            name='brand'
-            placeholder='Digite a marca do veículo'
-            value={form.values.brand}
+            name='description'
+            placeholder='Digite  a descrição do veículo'
+            value={form.values.description}
             onChange={form.handleChange}
             onBlur={form.handleBlur}
             className={styles.input}
           />
+          <p className={styles.error_message}>{form.errors.description}</p>
+        </div>
+
+        <div className={styles.input_wrapper}>
+          <label className={styles.label}>Marca</label>
+          <select
+            name='brand'
+            value={form.values.brand}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            className={styles.input}
+          >
+            <option hidden value=''>
+              Selecione a cor
+            </option>
+            {brandOptions.map(brand => (
+              <option value={brand} key={brand}>
+                {brand}
+              </option>
+            ))}
+          </select>
           <p className={styles.error_message}>{form.errors.brand}</p>
         </div>
         <div className={styles.input_wrapper}>
@@ -92,7 +133,9 @@ const CreateVehicle = () => {
               Selecione a cor
             </option>
             {colorOptions.map(color => (
-              <option value={color}>{color}</option>
+              <option value={color} key={color}>
+                {color}
+              </option>
             ))}
           </select>
           <p className={styles.error_message}>{form.errors.color}</p>
@@ -111,10 +154,23 @@ const CreateVehicle = () => {
           <p className={styles.error_message}>{form.errors.year}</p>
         </div>
         <div className={styles.input_wrapper}>
+          <label className={styles.label}>Preço</label>
+          <input
+            type='number'
+            name='price'
+            placeholder='Digite o preço do veículo'
+            value={form.values.price}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            className={styles.input}
+          />
+          <p className={styles.error_message}>{form.errors.price}</p>
+        </div>
+        <div className={styles.input_wrapper}>
           <label className={styles.label}>Placa</label>
           <input
             name='plate'
-            placeholder='Digite a placa do veículo'
+            placeholder='Ex. de placa válida: AAA1A33'
             value={form.values.plate}
             onChange={form.handleChange}
             onBlur={form.handleBlur}
