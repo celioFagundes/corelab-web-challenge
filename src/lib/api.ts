a02c4f5decb9209c83b55e32e5605802501c808d
Célio <celiofagundes30@gmail.com>
2022-07-08 22:56:01 -0300

    feat: created post

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import { IVehicleCreate } from '../types/Vehicle'
 const API = 'http://localhost:3333'
 const endpoint = (path: string): string => API + path
 
 const get = async (path: string): Promise<any> => {
   return axios.get(path).then(res => res.data)
+}
+const post = async (path: string, values: IVehicleCreate): Promise<any> => {
+  return axios.post(endpoint(path), values)
 }
 const put = async (path: string): Promise<any> => {
   return axios.put(endpoint(path))
@@ -13,6 +17,9 @@ const remove = async (path: string): Promise<any> => {
 }
 export const getVehicles = async (path: string) => {
   return get(path)
+}
+export const createVehicle = async ( values: IVehicleCreate) => {
+  return post('/vehicles',values)
 }
 export const removeVehicles = async (id: number) => {
   return remove(`/vehicles/${id}`)
